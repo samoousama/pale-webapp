5b3cef488f9b3fc2f93b831251964ef51a27d5d0
usama samoo <usamasamoo786@gmail.com>
2025-08-25 14:38:01 +0500

    working on it chagnes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Header from './components/Header/Navbar';
 import Button from "./components/Button/Button";
 
 import Fearures from "./components/Features/Features"
-import AboutMe from './components/Carousel/AboutMe';
 import PricePackages from './components/Pricing/PricingPackages';
 import FAQ from './components/FAQ/FAQ';
 import Footer from './components/Footer/Footer';
+import ChatInput from "./components/ChatInput/ChatCard";
 
 export const SIGNUP_FORM_LINK = "https://2fdzq6.share-eu1.hsforms.com/2pm4BhnAMQDSLDUgaF_OnAg"
 
@@ -56,21 +56,7 @@ function App() {
             </a>
           </div>  
           <div className='text-center mx-auto inline-flex flex-col justify-center pt-24'>
-            <div className='min-w-[645px] sm:min-w-[100%] mx-auto bg-[#ffffff66] py-5 border-[0.5px] px-5 rounded-[32px] border-white justify-between flex flex-col min-h-[150px] relative overflow-hidden backdrop-blur-sm shadow-lg'>
-             <div className="absolute w-full h-full bg-[#0000000f] top-0 left-0"></div>
-              <div className='w-full py-2 placeholder-black text-left bg-transparent border-transparent focus:border-transparent focus:ring-0 outline-none relative resize-none'>Create a draft pull request on <b>@GitHub</b> for my ticket on <b>@Jira</b></div>
-              <div className='flex justify-between items-center sm:gap-2'>
-                <div className='flex items-center gap-3 relative'>
-                  <img src='/attherat.svg' />
-                  <Button className='flex gap-1 !bg-[#d9d9d966] !text-primary border-[1px] border-white px-[5px] pr-[15px] py-[5px] items-center shadow-lg hover:!translate-y-[0px] sm:!text-[12px]'><img src='/github-icon.svg' className="sm:h-[16px]" />GitHub <img src='/cross-icon.svg'/></Button>
-                  <Button className='flex gap-1 !bg-[#d9d9d966] !text-primary border-[1px] border-white px-[5px] pr-[15px] py-[5px] items-center shadow-lg hover:!translate-y-[0px] sm:!text-[12px]'><img src='/jira-icon.svg' className="sm:h-[16px]" />Jira <img src='/cross-icon.svg'/></Button>
-                </div>
-                <div className='flex justify-between items-center gap-4 sm:gap-1'>
-                  <img src='/signIcon.svg' alt='' className='h-[16px]' />
-                  <Button className='w-[35px] h-[35px] !p-0 !flex !items-center !text-center justify-center !rounded-[100px]'><img src='/buttonArrow.svg' /></Button>
-                </div>
-              </div>
-            </div>
+            <ChatInput/>
             <img src='/hero-img.svg' />
           </div>
         </div>
